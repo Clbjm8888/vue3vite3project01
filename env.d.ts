8fa8ed97dc79d0b64ace830fa390967584537c00
libin <664229503@qq.com>
2022-08-21 19:52:18 +0800

    test01

---
+++ env.d.ts
@@ -0,0 +1,19 @@
+/// <reference types="vite/client" />
+
+//声明vue文件为一个module, 这样ts才能识别
+declare module "*.vue" {
+  import { DefineComponent } from "vue";
+  const component: DefineComponent;
+  export default component;
+}
+
+//声明js文件为一个module, 这样ts才能识别
+declare module "*.js";
+
+//动态添加到pinia的数据无法确定类型, 必须先在这里声明
+import "pinia";
+declare module "pinia" {
+  export interface PiniaCustomStateProperties {
+    abb: string;
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>{{ counter.counter }}</h2>
+    <h2>{{ counter.doubleCount }}</h2>
+    <button @click="counter.increment">increment</button>
+    <router-link to="/home">home</router-link>
+    <router-link to="/about">about</router-link>
+    <router-view></router-view>
+    <el-button>按扭1</el-button>
+    <el-button type="primary">按扭2</el-button>
+    <el-button type="warning" @click="btn">按扭3</el-button>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { useRouter } from "vue-router";
+  import { useCounterStore } from "./stores";
+  const counter = useCounterStore();
+  const router = useRouter();
+
+  const btn = () => {
+    router.push("/about");
+  };
+</script>
+
+<style scoped></style>
